ab8da143097f13a0e96df5c51cfad617ea32f5bd
Mahdi Qezlou <mahdiqezlou@gmail.com>
2023-03-29 19:20:46 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lali",
+    version="0.0.0",
+    author="Mahdi Qezlou, Simeon Bird, Adam Lidz, Guochao Sun, Andrew B. Newman",
+    author_email="mahdi.qezlou@email.ucr.edu",
+    description="Ly-Alpha forest x Line Intensity map forecast (LALI)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mahdiqezlou/lali",
+    project_urls={
+        "Bug Tracker": "https://github.com/mahdiqezlou/lali",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires="<=3.9",
+)
